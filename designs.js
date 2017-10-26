2e0cefde2ab7b71933f2af5ef1af94725ca421a3
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-10-26 21:22:37 +0100

    Remove form tag

--- designs.js
+++ designs.js
@@ -1,7 +1,12 @@
-let colorPicker; 
 
-colorPicker = $('#colorPicker');
+let color = $('#colorPicker').value;
 // Select color input
+let tableheight = $('#input_height').value;
+let tableWidth = $('input_width').value;
+console.log (tableWidth);
+console.log (tableheight);
+console.log (color);
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
