0426de08452687c68adeb60853febbb4e6a5c93d
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-10-26 21:28:58 +0100

    define all variables and nested loops

--- designs.js
+++ designs.js
@@ -1,18 +1,25 @@
 
-let color = $('#colorPicker').value;
 // Select color input
-let tableheight = $('#input_height').value;
-let tableWidth = $('input_width').value;
-console.log (tableWidth);
-console.log (tableheight);
-console.log (color);
 
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+ function makeGrid() {
 // Your code goes here!
+// Define all variable and select color nad size input
+   let color = $('#colorPicker').val();
+   let tableheight = $('#input_height').val();
+   let tableWidth = $('input_width').val();
+   let grid = "";
+
+// Nested loops to create individual boxes
+    for (let i=0; i < tableHeight; i++){
+    grid += "<tr>"
+    for(let m=0; m < tableWidth; m++){
+      grid += "<td></td>"
+    }
+    grid += "</tr>"
+    }
 
 }
